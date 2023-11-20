acadf0f90a1b0202b565e7ebc52064bc87635664
Victor Santos <vsantos.py@gmail.com>
2023-11-19 21:15:58 -0300

    fix: typo in aboutme

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
                 <h1>Hello there, I'm Victor</h1> 
                 
                 <h3>(as known as `vsantos`)</h3>
-                A production engineer (insert any other buzzwords here like 'DevOps') trying to learn a little bit of everything (and failing for obvious reasons). I have solid knowledge in very topic listed at `/skils` <br/>
+                A production engineer (insert any other buzzwords here like 'DevOps') trying to learn a little bit of everything (and failing for obvious reasons). I have solid knowledge in every topic listed at `/skils` <br/>
                 Feel free to get in touch to exchange experiences. (:
             </div>
 
